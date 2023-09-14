422f3db1b0ffe8d16802fdd86d2853e06c396a87
Jeremy Switlyk <jeremy.switlyk@protonmail.ch>
2023-09-14 17:49:50 -0400

    Create Contact on Form Submission

--- web/src/pages/ContactPage/ContactPage.jsx
+++ web/src/pages/ContactPage/ContactPage.jsx
@@ -6,11 +6,20 @@ import {
   Submit,
   FieldError,
 } from '@redwoodjs/forms'
-import { MetaTags } from '@redwoodjs/web'
+import { MetaTags, useMutation } from '@redwoodjs/web'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
 
 const ContactPage = () => {
+  const [create] = useMutation(CREATE_CONTACT)
   const onSubmit = (data) => {
-    console.log(data)
+    create({ variables: { input: data } })
   }
 
   return (
