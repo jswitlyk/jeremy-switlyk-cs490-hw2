edded88ac5187c99860e996fda962e36d34885e3
Jeremy Switlyk <jeremy.switlyk@protonmail.ch>
2023-09-14 17:10:34 -0400

    Display Error Messages for Missing Text Fields

--- web/src/pages/ContactPage/ContactPage.jsx
+++ web/src/pages/ContactPage/ContactPage.jsx
@@ -1,4 +1,10 @@
-import { Form, TextField, TextAreaField, Submit } from '@redwoodjs/forms'
+import {
+  Form,
+  TextField,
+  TextAreaField,
+  Submit,
+  FieldError,
+} from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 
 const ContactPage = () => {
@@ -7,18 +13,23 @@ const ContactPage = () => {
   }
 
   return (
-    <Form onSubmit={onSubmit}>
-      <label htmlFor="name">Name</label>
-      <TextField name="name" validation={{ required: true }} />
+    <>
+      <Form onSubmit={onSubmit}>
+        <label htmlFor="name">Name</label>
+        <TextField name="name" validation={{ required: true }} />
+        <FieldError name="name" />
 
-      <label htmlFor="email">Email</label>
-      <TextField name="email" validation={{ required: true }} />
+        <label htmlFor="email">Email</label>
+        <TextField name="email" validation={{ required: true }} />
+        <FieldError name="email" />
 
-      <label htmlFor="message">Message</label>
-      <TextAreaField name="message" validation={{ required: true }} />
+        <label htmlFor="message">Message</label>
+        <TextAreaField name="message" validation={{ required: true }} />
+        <FieldError name="message" />
 
-      <Submit>Save</Submit>
-    </Form>
+        <Submit>Save</Submit>
+      </Form>
+    </>
   )
 }
 
