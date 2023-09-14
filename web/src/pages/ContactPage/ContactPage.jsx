4fc9a7809e048f2060c61e2021aa62adec4bcbe9
Jeremy Switlyk <jeremy.switlyk@protonmail.ch>
2023-09-14 16:52:52 -0400

    Log Submitted Data on Contact Page

--- web/src/pages/ContactPage/ContactPage.jsx
+++ web/src/pages/ContactPage/ContactPage.jsx
@@ -2,11 +2,15 @@ import { Form, TextField, Submit } from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 
 const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form>
+      <Form onSubmit={onSubmit}>
         <TextField name="input" />
         <Submit>Save</Submit>
       </Form>
